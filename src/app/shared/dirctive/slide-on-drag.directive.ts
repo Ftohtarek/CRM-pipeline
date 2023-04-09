a617ab4d17d169fb901036234fb2e930e7e428dd
unknown <ftohtarek48@gmail.com>
2023-04-09 05:15:14 +0200

    update and refactor in slideOnDrag dirctive to have free swap in mobile mode

--- src/app/shared/dirctive/slide-on-drag.directive.ts
+++ src/app/shared/dirctive/slide-on-drag.directive.ts
@@ -1,40 +1,46 @@
 import { AfterViewInit, Directive, ElementRef, HostListener } from '@angular/core';
-
 @Directive({
   selector: '[slideOnDrag]'
 })
-export class SlideOnDragDirective implements AfterViewInit {
-  draggableArea = ['statge', 'section', 'heading', "summary", 'h2']
+export class SlideOnDragDirective {
+
   Ready: boolean = false
-  constructor(private el: ElementRef<HTMLElement>) { }
-  ngAfterViewInit(): void {
+  private startX = 0
 
+  dragElement!: HTMLElement | null
+  constructor(private el: ElementRef<HTMLElement>) {
+    document.body.addEventListener('touchmove', (e: TouchEvent) => {
+      if (this.dragElement) {
+        let distance = (e.changedTouches[0].clientX - this.startX) / 20
+        this.el.nativeElement.scrollLeft += distance
+      }
+    })
+  }
 
-  }
-  private canSlide(el: HTMLElement) {
-    let indicator = false
-    this.draggableArea.forEach(areaName =>
-      el.className.includes(areaName) || el.tagName.match(areaName) ? indicator = true : false
-    )
-    return indicator
+  @HostListener('touchstart', ['$event'])
+  @HostListener('mousedown', ['$event'])
+  touchStart(e: TouchEvent) {
+    if (e instanceof TouchEvent) this.startX = e.changedTouches[0].clientX
+    this.dragElement = (<HTMLElement>e.target).closest('.host')
+    this.Ready = this.el.nativeElement.scrollWidth == this.el.nativeElement.clientWidth ? false : true
   }
 
-  @HostListener('click', ['$event']) onclick(event: MouseEvent) {
-  }
-  @HostListener('mousedown', ['$event']) touchStart(event: MouseEvent) {
-
-    this.Ready = this.el.nativeElement.scrollWidth == this.el.nativeElement.clientWidth
-      || !this.canSlide(<HTMLElement>event.target) ? false : true
-  }
-  @HostListener('mouseup', ['$event']) touchEnd(event: MouseEvent) {
+  @HostListener('mouseup')
+  @HostListener('touchend')
+  touchEnd() {
     this.Ready = false
     this.el.nativeElement.classList.remove('crm-slideOnDrag')
-
   }
-  @HostListener('mousemove', ['$event']) moveStart(event: MouseEvent) {
+
+  @HostListener('mousemove', ['$event'])
+  moveStart(event: MouseEvent) {
     if (this.Ready) {
+      let distanceX = (<MouseEvent>event).movementX;
+      this.el.nativeElement.scrollLeft += this.dragElement ? distanceX : -distanceX
       this.el.nativeElement.classList.add('crm-slideOnDrag')
-      this.el.nativeElement.scrollLeft -= event.movementX
     }
   }
+
 }
+
+
